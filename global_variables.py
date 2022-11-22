2978f8c60ff96f4a05c50c4161b9f527252b0f1c
tanguy_lunel <tanguy.lunel@meteo.fr>
2022-11-22 15:47:42 +0100

    * addition of module made to contain the variables used in different scripts (ex: sites coordinates, paths, etc)

---
+++ global_variables.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: lunelt
+
+Gathers global variables for use in scripts. 
+"""
+
+sites = {'cendrosa': {'lat': 41.6925905,
+                      'lon': 0.9285671},
+         'preixana': {'lat': 41.59373,
+                      'lon': 1.07250},
+         'elsplans': {'lat': 41.590111,
+                      'lon': 1.029363},
+        'tossal baltasana': {'lat': 41.3275,
+                                  'lon': 1.00336},
+             'puig formigosa': {'lat': 41.42179,
+                                'lon': 1.44177},
+             'tossal gros': {'lat': 41.47857,
+                             'lon': 1.12942},
+             'tossal torretes': {'lat': 42.02244,
+                                 'lon': 0.93800},
+             'moncayo': {'lat': 41.7871,
+                         'lon': -1.8396},
+             'tres mojones': {'lat': 40.75887,
+                              'lon': -0.63924},
+             'guara': {'lat': 42.28865,
+                       'lon': -0.22992},
+             'caro': {'lat': 40.80332,
+                      'lon': 0.34325},
+             'montserrat': {'lat': 41.6052,
+                            'lon': 1.81182},
+             'joar': {'lat': 42.6345,
+                      'lon': -2.34898},
+        }
+         
+mountains = {'tossal baltasana': {'lat': 41.3275,
+                                  'lon': 1.00336},
+             'puig formigosa': {'lat': 41.42179,
+                                'lon': 1.44177},
+             'tossal gros': {'lat': 41.47857,
+                             'lon': 1.12942},
+             'tossal torretes': {'lat': 42.02244,
+                                 'lon': 0.93800},
+             'moncayo': {'lat': 41.7871,
+                         'lon': -1.8396},
+             'tres mojones': {'lat': 40.75887,
+                              'lon': -0.63924},
+             'guara': {'lat': 42.28865,
+                       'lon': -0.22992},
+             'caro': {'lat': 40.80332,
+                      'lon': 0.34325},
+             'montserrat': {'lat': 41.6052,
+                            'lon': 1.81182},
+             'joar': {'lat': 42.6345,
+                      'lon': -2.34898},
+             'coll_lilla': {'lat': 41.34072,
+                            'lon': 1.22014},
+             'puig_pelat': {'lat': 41.26571,
+                            'lon': 1.05502},           
+            }
+
+#site_start = 'arbeca'
+#start = (41.54236, 0.9232)
+#site_start = 'verdu'
+#start = (41.6107, 1.1428)
+#site_end = 'tarragona'
+#end = (41.1188, 1.2456)
+#
+#site_end = 'st_maria'
+#end = (41.36315, 1.29917)
+         
+#site_start = 'preixana'
+#start = (41.59373, 1.0725)
+#start = (sites[site_start]['lat'], sites[site_start]['lon'])
