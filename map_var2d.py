da8a159c86a0a11398d7a00889451e60ef75dda5
tanguy_lunel <tanguy.lunel@meteo.fr>
2022-11-22 15:45:07 +0100

    * border between france and spain added in maps * mountains peaks added on maps * some general improvement on versatility of code

--- map_var2d.py
+++ map_var2d.py
@@ -3,95 +3,164 @@
 @author: tylunel
 Creation : 07/01/2021
 
-Multiple attempts to use MetPy BarbPlot(), but multiple issues.
-Best option seems to be the simplest as follows:
+Script for plotting simple colormaps 
 """
 
-#import os
 import matplotlib.pyplot as plt
-import numpy as np
 import xarray as xr
-import metpy.calc as mpcalc
-from metpy.units import units
-import pandas as pd 
-from tools import indices_of_lat_lon, get_simu_filename
+import tools
+import shapefile
+import pandas as pd
+import global_variables
 
 ###############################################
-model = 'irr'
-#ilevel = 2  #0 is Halo, 1->2m, 2->6.12m, 3->10.49m, 10->49.3m
+model = 'irr_d1'
+
+domain_nb = 1
+
+wanted_date = '20210729-2300'
+
+color_map = 'jet'    # BuPu, coolwarm, viridis, RdYlGn, jet
+
+var_name = 'LE_ISBA'   #LAI_ISBA, ZO_ISBA, PATCHP7, ALBNIR_S, MSLP, TG1_ISBA, RAINF_ISBA, CLDFR
+vmin=-300
+vmax=600
+
+# level, only useful if var 3D
+ilevel = 1  #0 is Halo, 1:2m, 2:6.12m, 3:10.49m, 10:49.3m, 20:141m, 30:304m, 40:600m, 50:1126m, 60:2070m
+
+zoom_on = None  #None for no zoom, 'liaise' or 'urgell'
+
+if zoom_on == 'liaise':
+    skip_barbs = 3 # 1/skip_barbs will be printed
+    barb_length = 5.5
+    lat_range = [41.45, 41.8]
+    lon_range = [0.7, 1.2]
+elif zoom_on == 'urgell':
+    skip_barbs = 6 # 1/skip_barbs will be printed
+    barb_length = 4.5
+    lat_range = [41.1, 42.1]
+    lon_range = [0.2, 1.7]
+    
+
 save_plot = True
-save_folder = './figures/pgd_maps/'
+save_folder = './figures/scalar_maps/domain{0}/{1}/'.format(
+        domain_nb, var_name)
 
-color_map = 'RdYlGn'    # BuPu, coolwarm, viridis, RdYlGn
-
-var_name = 'LAI_ISBA'   #LAI_ISBA, ZO_ISBA, PATCHP7, ALBNIR_S
 ##############################################
 
-
-filename = get_simu_filename(model)
+if domain_nb == 1:
+    filename = tools.get_simu_filename_d1(model, wanted_date)
+else:
+    filename = tools.get_simu_filename(model, wanted_date)
 
 # load dataset, default datetime okay as pgd vars are all the same along time
 ds1 = xr.open_dataset(filename)
+#ds1 = xr.open_dataset('/cnrm/surface/lunelt/NO_SAVE/nc_out/2.01_pgds_irr/' + \
+#        'PGD_400M_CovCor_v26_ivars.nc')
 
 
 #%% DATA SELECTION and ZOOM
 
-var2d = ds1[var_name]
+varNd = ds1[var_name]
+#remove single dimensions
+varNd = varNd.squeeze()
+
+if len(varNd.shape) == 2:
+    var2d = varNd
+elif len(varNd.shape) == 3:
+    var2d = varNd[ilevel,:,:]
+    
 # remove 999 values, and replace by nan
 var2d = var2d.where(~(var2d == 999))
+# filter the outliers
+var2d = var2d.where(var2d <= vmax)
+
+
 
 
 #%% PLOT OF VAR_NAME
-fig1 = plt.figure(figsize=(7,5))
+if domain_nb == 1:
+    fig1 = plt.figure(figsize=(13,7))
+elif domain_nb == 2:
+    fig1 = plt.figure(figsize=(10,7))
 
-plt.pcolormesh(var2d.longitude, var2d.latitude, var2d,
+#plt.pcolormesh(var2d.longitude, var2d.latitude, var2d,
+##               cbar_kwargs={"orientation": "horizontal", "shrink": 0.7}
+#               cmap=color_map,
+#               vmin=vmin, vmax=vmax
+#               )
+
+plt.contourf(var2d.longitude, var2d.latitude, var2d,
 #               cbar_kwargs={"orientation": "horizontal", "shrink": 0.7}
                cmap=color_map,
-               vmin=-1, vmax=4
+               vmin=vmin, vmax=vmax,
+               levels=20
                )
-cbar = plt.colorbar()
-cbar.set_label(var2d.long_name)
 
-#%% IRRIGATED COVERS
-domain = '400m'
-if domain == '400m':
+cbar = plt.colorbar(boundaries=[vmin, vmax])
+cbar.set_label(var2d.long_name)
+#cbar.set_clim(vmin, vmax)
+
+#%% IRRIGATED, SEA and COUNTRIES BORDERS
+
+if domain_nb == 2:
     pgd = xr.open_dataset(
         '/cnrm/surface/lunelt/NO_SAVE/nc_out/2.01_pgds_irr/' + \
         'PGD_400M_CovCor_v26_ivars.nc')
-elif domain == '2km':
+elif domain_nb == 1:
     pgd = xr.open_dataset(
         '/cnrm/surface/lunelt/NO_SAVE/nc_out/2.01_pgds_irr/' + \
         'PGD_2KM_CovCor_v26_ivars.nc')
 
+#Irrigation borders
 #from scipy.ndimage.filters import gaussian_filter
 #sigma = 0.1     #default is 0.1
 #irr_covers = gaussian_filter(pgd.COVER369.data, sigma)
 irr_covers = pgd.COVER369.data
-
 plt.contour(pgd.longitude.data, 
-          pgd.latitude.data, 
-          irr_covers,
-          levels=0,   #+1 -> number of contour to plot 
-          linestyles='solid',
-          linewidths=1.,
-          colors='g'
-#          colors=['None'],
-#          hatches='-'
-          )
+            pgd.latitude.data, 
+            irr_covers,
+            levels=0,   #+1 -> number of contour to plot 
+            linestyles='solid',
+            linewidths=1.,
+            colors='g'
+#            colors=['None'],
+#            hatches='-'
+            )
+
+#Sea borders
+sea_covers = pgd.COVER001.data
+plt.contour(pgd.longitude.data, 
+            pgd.latitude.data, 
+            sea_covers,
+            levels=0,   #+1 -> number of contour to plot 
+            linestyles='solid',
+            linewidths=1.,
+            colors='k'
+    #        colors=['None'],
+    #        hatches='-'
+            )
+
+#France borders
+sf = shapefile.Reader("TM-WORLD-BORDERS/TM_WORLD_BORDERS-0.3.sph")
+shapes=sf.shapes()
+france = shapes[64].points
+france_df = pd.DataFrame(france, columns=['lon', 'lat'])
+france_S = france_df[france_df.lat < 43.35]
+france_SW = france_S[france_S.lon < 2.95]
+plt.plot(france_SW.lon, france_SW.lat,
+         color='k',
+         linewidth=1)
 
 #%% POINTS SITES
-sites = {'cendrosa': {'lat': 41.6925905,
-                      'lon': 0.9285671},
-         'preixana': {'lat': 41.59373,
-                      'lon': 1.07250},
-         'elsplans': {'lat': 41.590111,
-                      'lon': 1.029363},
-        }
+
+sites = global_variables.sites
 
 for site in sites:
     plt.scatter(sites[site]['lon'],
                 sites[site]['lat'],
-                color='r',
+                color='k',
                 s=10        #size of markers
                 )
     if site == 'elsplans':
@@ -107,17 +176,19 @@ for site in sites:
 
 
 #%% FIGURE OPTIONS and ZOOM
-plot_title = '{0} for simu {1}'.format(
-        var_name,
-        model)
+if len(varNd.shape) == 2:
+    plot_title = '{0} - {1} for simu {2}'.format(
+        wanted_date, var_name, model)
+elif len(varNd.shape) == 3:
+    plot_title = '{0} - {1} for simu {2} at {3}m'.format(
+        wanted_date, var_name, model, var2d.level.round())
+
 plt.title(plot_title)
 
-plt.ylim([41.4, 42.0])
-plt.xlim([0.4, 1.4])
+if zoom_on is not None:
+    plt.ylim(lat_range)
+    plt.xlim(lon_range)
 
 
 if save_plot:
-    filename = (plot_title)
-    filename = filename.replace('=', '').replace('(', '').replace(')', '')
-    filename = filename.replace(' ', '_').replace(',', '').replace('.', '_')
-    plt.savefig(save_folder+str(filename))
+    tools.save_figure(plot_title, save_folder)
--- map_wind.py
+++ map_wind.py
@@ -7,7 +7,6 @@ Multiple attempts to use MetPy BarbPlot(), but multiple issues.
 Best option seems to be the simplest as follows:
 """
 
-#import os
 import matplotlib.pyplot as plt
 import numpy as np
 import xarray as xr
@@ -15,20 +14,22 @@ import metpy.calc as mpcalc
 from metpy.units import units
 import pandas as pd 
 import tools
-
+import shapefile
 
 #########################################"""
-model = 'irr'
-ilevel = 3  #0 is Halo, 1:2m, 2:6.1m, 3:10.5m, 10:49.3m, 20:141m, 30:304m, 40:600m, 50:1126m, 60:2070
-skip_barbs = 10 # 1/skip_barbs will be printed
-barb_length = 4
+model = 'irr_d1'
+ilevel = 50  #0 is Halo, 1:2m, 2:6.1m, 3:10.5m, 10:49.3m, 20:141m, 30:304m, 40:600m, 50:1126m, 60:2070
+skip_barbs = 5 # 1/skip_barbs will be printed
+barb_length = 4.5
 # Datetime
-wanted_date = '20210723-1200'
+wanted_date = '20210729-2300'
 
-speed_plane = 'horiz'  # 'horiz': horizontal 'normal' wind, 'verti' for W
+domain_nb = 1
+
+speed_plane = 'verti'  # 'horiz': horizontal 'normal' wind, 'verti' for W
 
 if speed_plane == 'verti':
-    vmax_cbar = 4
+    vmax_cbar = 5
     vmin_cbar = -vmax_cbar
     cmap_name = 'seismic'
 elif speed_plane == 'horiz':
@@ -50,20 +51,25 @@ elif zoom_on == 'urgell':
     lon_range = [0.2, 1.7]
     
     
-save_plot = True
-save_folder = './figures/winds/horiz/'
+save_plot = False
+save_folder = './figures/winds/domain{0}/{1}/{2}/'.format(
+        domain_nb, speed_plane, ilevel)
+
+barb_size_option = 'weak_winds'  # 'weak_winds' or 'standard'
+
 
 ###########################################
 
-simu_folders = {
-        'irr': '2.13_irr_2021_22-27/', 
-        'std': '1.11_ECOII_2021_ecmwf_22-27/'
-         }
-father_folder = '/cnrm/surface/lunelt/NO_SAVE/nc_out/'
+barb_size_increments = {
+        'weak_winds': {'half':1.94, 'full':3.88, 'flag':19.4},
+        'standard': {'half':5, 'full':10, 'flag':50},
+        }
+barb_size_description = {
+        'weak_winds': "barb increments: half=1m/s=1.94kt, full=2m/s=3.88kt, flag=10m/s=19.4kt",
+        'standard': "barb increments: half=5kt=2.57m/s, full=10kt=5.14m/s, flag=50kt=25.7m/s",
+        }
 
-datafolder = father_folder + simu_folders[model]
-
-filename = tools.get_simu_filename(model, wanted_date)
+filename = tools.get_simu_filename_d1(model, wanted_date)
 
 # load file, dataset and set parameters
 ds1 = xr.open_dataset(filename,
@@ -73,7 +79,10 @@ ds1 = xr.open_dataset(filename,
 #                      grid_mapping=latitude
                       )
 
-fig1 = plt.figure(figsize=(13,9))
+if domain_nb == 1:
+    fig1 = plt.figure(figsize=(17,9))
+elif domain_nb == 2:
+    fig1 = plt.figure(figsize=(13,9))
 
 #%% WIND SPEED COLORMAP
 if speed_plane == 'horiz':
@@ -110,40 +119,83 @@ plt.barbs(X[::skip_barbs, ::skip_barbs], Y[::skip_barbs, ::skip_barbs],
 #                 'spacing':1, 
 #                 'height':1,
 #                 'width':1,
-                 'emptybarb':0.01}
+                 'emptybarb':0.01},
+          barb_increments=barb_size_increments[barb_size_option]
           )
+plt.annotate(barb_size_description[barb_size_option],
+             xy=(0.1, 0.05),
+             xycoords='subfigure fraction'
+             )
 
 
-#%% IRRIGATED COVERS
+#%% IRRIGATED, SEA and COUNTRIES BORDERS
 
-pgd = xr.open_dataset(
-    '/cnrm/surface/lunelt/NO_SAVE/nc_out/2.01_pgds_irr/' + \
-    'PGD_400M_CovCor_v26_ivars.nc')
+if domain_nb == 2:
+    pgd = xr.open_dataset(
+        '/cnrm/surface/lunelt/NO_SAVE/nc_out/2.01_pgds_irr/' + \
+        'PGD_400M_CovCor_v26_ivars.nc')
+elif domain_nb == 1:
+    pgd = xr.open_dataset(
+        '/cnrm/surface/lunelt/NO_SAVE/nc_out/2.01_pgds_irr/' + \
+        'PGD_2KM_CovCor_v26_ivars.nc')
 
+#Irrigation borders
 #from scipy.ndimage.filters import gaussian_filter
 #sigma = 0.1     #default is 0.1
 #irr_covers = gaussian_filter(pgd.COVER369.data, sigma)
 irr_covers = pgd.COVER369.data
-
 plt.contour(pgd.longitude.data, 
-          pgd.latitude.data, 
-          irr_covers,
-          levels=0,   #+1 -> number of contour to plot 
-          linestyles='solid',
-          linewidths=1.,
-          colors='g'
-#          colors=['None'],
-#          hatches='-'
-          )
+            pgd.latitude.data, 
+            irr_covers,
+            levels=0,   #+1 -> number of contour to plot 
+            linestyles='solid',
+            linewidths=1.,
+            colors='g'
+#            colors=['None'],
+#            hatches='-'
+            )
+
+#Sea borders
+sea_covers = pgd.COVER001.data
+plt.contour(pgd.longitude.data, 
+            pgd.latitude.data, 
+            sea_covers,
+            levels=0,   #+1 -> number of contour to plot 
+            linestyles='solid',
+            linewidths=1.,
+            colors='k'
+    #        colors=['None'],
+    #        hatches='-'
+            )
+
+#France borders
+sf = shapefile.Reader("TM-WORLD-BORDERS/TM_WORLD_BORDERS-0.3.sph")
+shapes=sf.shapes()
+france = shapes[64].points
+france_df = pd.DataFrame(france, columns=['lon', 'lat'])
+france_S = france_df[france_df.lat < 43.35]
+france_SW = france_S[france_S.lon < 2.95]
+plt.plot(france_SW.lon, france_SW.lat,
+         color='k',
+         linewidth=1)
 
 #%% POINTS SITES
-sites = {'cendrosa': {'lat': 41.6925905,
-                      'lon': 0.9285671},
-         'preixana': {'lat': 41.59373,
-                      'lon': 1.07250},
-         'elsplans': {'lat': 41.590111,
-                      'lon': 1.029363},
-        }
+#sites = {'cendrosa': {'lat': 41.6925905,
+#                      'lon': 0.9285671},
+##         'preixana': {'lat': 41.59373,
+##                      'lon': 1.07250},
+#         'elsplans': {'lat': 41.590111,
+#                      'lon': 1.029363},
+#         'tossal baltasana': {'lat': 41.3275,
+#                              'lon': 1.00336},
+#         'puig formigosa': {'lat': 41.42179,
+#                            'lon': 1.44177},
+#         'tossal gros': {'lat': 41.47857,
+#                         'lon': 1.12942},
+#         'tossal torretes': {'lat': 42.02244,
+#                             'lon': 0.93800}
+#        }
+sites = tools.sites
 
 for site in sites:
     plt.scatter(sites[site]['lon'],
@@ -155,12 +207,12 @@ for site in sites:
         plt.text(sites[site]['lon']-0.1,
                  sites[site]['lat']-0.03, 
                  site, 
-                 fontsize=12)
+                 fontsize=9)
     else:
         plt.text(sites[site]['lon']+0.01,
                  sites[site]['lat']+0.01, 
                  site, 
-                 fontsize=12)
+                 fontsize=9)
 
 
 #%% FIGURE OPTIONS
