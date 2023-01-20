7ffce426302dddf4f4616737d954f08b38b76cfd
tanguy_lunel <tanguy.lunel@meteo.fr>
2023-01-20 12:00:59 +0100

    * new script 'plot_cross_section_diff' for computing difference in 2 cross_section * general cleaning

--- plot_cross_section_diff.py
+++ plot_cross_section_diff.py
@@ -28,7 +28,7 @@ surf_var_label = 'Q_vol_soil'
 # Set type of wind representation: 'verti_proj' or 'horiz'
 wind_visu = 'verti_proj'
 # Datetime
-wanted_date = '20210722-2300'
+wanted_date = '20210722-1200'
 # altitude ASL or height AGL: 'asl' or 'agl'
 alti_type = 'asl'
 # maximum level (height AGL) to plot
@@ -50,8 +50,10 @@ barb_size_option = 'weak_winds'  # 'weak_winds' or 'standard'
 # Save the figure
 figsize = (15,9)
 save_plot = True
+simu_folders = ['irr_d2', 'std_d2']
+folder_res = 'diff_{0}_{1}'.format(simu_folders[0], simu_folders[1])
 save_folder = './figures/cross_sections/{0}/section_{1}_{2}/{3}/'.format(
-        model, site_start, site_end, wind_visu)
+        folder_res, site_start, site_end, wind_visu)
 
 
 ###########################################
@@ -69,81 +71,88 @@ barb_size_description = {
 end = (gv.sites[site_end]['lat'], gv.sites[site_end]['lon'])
 start = (gv.sites[site_start]['lat'], gv.sites[site_start]['lon'])
 
-# Dependant parameters
-filename = tools.get_simu_filename(model, wanted_date, 
-#                                      domain=domain_nb,
-#                                      file_suffix='001dg'
-                                      )
 
-#load file
-data_perso = xr.open_dataset(filename)
-data_reduced = data_perso[['THT', 'RVT', 'UT', 'VT', 'WT', 'ZS',
-                           'TEMP', 'PRES',
-                           surf_var]]
-data = data_reduced
+section_ds = {}
 
-
-#%% Put variables U, V, W in the middle of the grid:
-
-# with external function:
-data = tools.center_uvw(data)
-
-
-#%% CREATE SECTION LINE
-
-line = tools.line_coords(data, start, end, 
-                         nb_indices_exterior=nb_points_beyond)
-ni_range = line['ni_range']
-nj_range = line['nj_range']
-slope = line['slope']
-
-if slope == 'vertical':
-    angle = np.pi/2
-else:
-    angle = np.arctan(slope)
+for model in simu_folders:
+    # Dependant parameters
+    filename = tools.get_simu_filename(model, wanted_date, 
+    #                                      domain=domain_nb,
+    #                                      file_suffix='001dg'
+                                          )
     
-data['PROJ'] = misc.windvec_verti_proj(data['UT'], data['VT'], 
-                                       data.level, angle)
-
-#%% INTERPOLATION
-
-section = []
-abscisse_coords = []
-abscisse_sites = {}
-
-#get total maximum height of relief on domain
-max_ZS = data['ZS'].max()
-level_range = np.arange(10, toplevel+max_ZS, 10)
-
-print('section interpolation on {0} points (~1sec/pt)'.format(len(ni_range)))
-for i, ni in enumerate(ni_range):
-    nj=nj_range[i]
-    #interpolation of all variables on ni_range
-    profile = data.interp(ni=ni, 
-                          nj=nj, 
-                          level=level_range).expand_dims({'i_sect':[i]})
-    section.append(profile)
+    #load file
+    data_perso = xr.open_dataset(filename)
+    data_reduced = data_perso[['THT', 'RVT', 'UT', 'VT', 'WT', 'ZS',
+                               'TEMP', 'PRES',
+                               surf_var]]
+    data = data_reduced
     
-    #store values of lat-lon for the horiz axis
-    lat = np.round(profile.latitude.values, decimals=3)
-    lon = np.round(profile.longitude.values, decimals=3)
-    latlon = str(lat) + '\n' + str(lon)
-    abscisse_coords.append(latlon)
     
-    #Store values of i and name of site in dict for horiz axis
+    #%% Put variables U, V, W in the middle of the grid:
+    
+    # with external function:
+    data = tools.center_uvw(data)
+    
+    
+    #%% CREATE SECTION LINE
+    
+    line = tools.line_coords(data, start, end, 
+                             nb_indices_exterior=nb_points_beyond)
+    ni_range = line['ni_range']
+    nj_range = line['nj_range']
+    slope = line['slope']
+    
     if slope == 'vertical':
-        if nj == line['nj_start']:
-            abscisse_sites[i] = site_start
-        elif nj == line['nj_end']:
-            abscisse_sites[i] = site_end
+        angle = np.pi/2
     else:
-        if ni == line['ni_start']:
-            abscisse_sites[i] = site_start
-        elif ni == line['ni_end']:
-            abscisse_sites[i] = site_end
+        angle = np.arctan(slope)
+        
+    data['PROJ'] = misc.windvec_verti_proj(data['UT'], data['VT'], 
+                                           data.level, angle)
+    
+    #%% INTERPOLATION
+    
+    section = []
+    abscisse_coords = []
+    abscisse_sites = {}
+    
+    #get total maximum height of relief on domain
+    max_ZS = data['ZS'].max()
+    level_range = np.arange(10, toplevel+max_ZS, 10)
+    
+    print('section interpolation on {0} points (~1sec/pt)'.format(len(ni_range)))
+    for i, ni in enumerate(ni_range):
+        nj=nj_range[i]
+        #interpolation of all variables on ni_range
+        profile = data.interp(ni=ni, 
+                              nj=nj, 
+                              level=level_range).expand_dims({'i_sect':[i]})
+        section.append(profile)
+        
+        #store values of lat-lon for the horiz axis
+        lat = np.round(profile.latitude.values, decimals=3)
+        lon = np.round(profile.longitude.values, decimals=3)
+        latlon = str(lat) + '\n' + str(lon)
+        abscisse_coords.append(latlon)
+        
+        #Store values of i and name of site in dict for horiz axis
+        if slope == 'vertical':
+            if nj == line['nj_start']:
+                abscisse_sites[i] = site_start
+            elif nj == line['nj_end']:
+                abscisse_sites[i] = site_end
+        else:
+            if ni == line['ni_start']:
+                abscisse_sites[i] = site_start
+            elif ni == line['ni_end']:
+                abscisse_sites[i] = site_end
+    
+    #concatenation of all profile in order to create the 2D section dataset
+    section_ds[model] = xr.concat(section, dim="i_sect")
 
-#concatenation of all profile in order to create the 2D section dataset
-section_ds = xr.concat(section, dim="i_sect")
+section_diff = section_ds[simu_folders[0]] - section_ds[simu_folders[1]]
+section_diff['ZS'] = section_ds[simu_folders[0]]['ZS']
 
 #%% PLOT
 # create figure
@@ -152,25 +161,25 @@ fig, ax = plt.subplots(2, figsize=figsize,
 
 ## --- Subplot of section, i.e. the main plot ----
 #get maximum height of relief in cross-section
-max_ZS = section_ds['ZS'].max()
+max_ZS = section_diff['ZS'].max()
 
 # remove top layers of troposphere
-section_ds = section_ds.where(section_ds.level<(toplevel+max_ZS), drop=True)
+section_diff = section_diff.where(section_diff.level<(toplevel+max_ZS), drop=True)
 
 ## --- Adapt to alti_type ------
 #create grid mesh (eq. to X)
-X = np.meshgrid(section_ds.i_sect, section_ds.level)[0]
+X = np.meshgrid(section_diff.i_sect, section_diff.level)[0]
 Xmesh = xr.DataArray(X, dims=['level', 'i_sect'])
 #create alti mesh (eq. to Y)
 if alti_type == 'asl': 
     #compute altitude ASL from height AGL, and transpose (eq. Y)
-    alti = section_ds.ZS[:, 0] + section_ds.level
+    alti = section_diff.ZS[:, 0] + section_diff.level
     alti = alti.T
     #for plot
     ylabel = 'altitude ASL [m]'
 elif alti_type == 'agl':
     #create grid mesh (eq. Y)
-    alti = np.meshgrid(section_ds.i_sect, section_ds.level)[1]
+    alti = np.meshgrid(section_diff.i_sect, section_diff.level)[1]
     alti = xr.DataArray(alti, dims=['level', 'i_sect'])
     #for plot
     ylabel = 'height AGL [m]'
@@ -178,13 +187,13 @@ elif alti_type == 'agl':
 
 #%% Computation of diagnostic variable
     
-section_ds['DENS'] = calc.density(
-    section_ds['PRES']*units.hectopascal,
-    section_ds['THT']*units.K, 
-    section_ds['RVT']*units.gram/units.gram)
+section_diff['DENS'] = calc.density(
+    section_diff['PRES']*units.hectopascal,
+    section_diff['THT']*units.K, 
+    section_diff['RVT']*units.gram/units.gram)
 
 ### 1. Color map (pcolor or contourf)
-data1 = section_ds['THT']
+data1 = section_diff['THT']
 #cm = ax[0].pcolormesh(Xmesh,
 #                      alti,
 #                      data1.T, 
@@ -194,10 +203,10 @@ cm = ax[0].contourf(Xmesh,
                     alti,
                     data1.T, 
                     cmap='rainbow',
-                    levels=np.linspace(298, 315, 18),  # to keep always same colorbar limits
+                    levels=np.linspace(-5, 5, 11),  # to keep always same colorbar limits
 #                    levels=20,
 #                    extend = 'both',  #highlights the min and max in different color
-                    vmin=298, vmax=315,  # for THT
+                    vmin=-5, vmax=5,  # for THT
 #                    vmin=None, vmax=None,  # for adaptative colormap
 #                    vmin=800, vmax=1000,  # for PRES
                     )
@@ -209,7 +218,7 @@ cbar.set_label('theta [K]')
 
 
 ### 2. Contour map
-data2 = section_ds['RVT']
+data2 = section_diff['RVT']
 cont = ax[0].contour(Xmesh,
                      alti,
                      data2.T)
@@ -222,8 +231,8 @@ if wind_visu == 'horiz':            # 2.1 winds - flat direction and force
             Xmesh[::skip_barbs_y, ::skip_barbs_x], 
             alti[::skip_barbs_y, ::skip_barbs_x], 
             #Here dimensions are in the proper order
-            section_ds['UT'][::skip_barbs_x, ::skip_barbs_y].T, 
-            section_ds['VT'][::skip_barbs_x, ::skip_barbs_y].T, 
+            section_diff['UT'][::skip_barbs_x, ::skip_barbs_y].T, 
+            section_diff['VT'][::skip_barbs_x, ::skip_barbs_y].T, 
             pivot='middle',
             length=5*arrow_size,     #length of barbs
             sizes={
@@ -241,13 +250,13 @@ elif wind_visu == 'verti_proj':     # 2.2  winds - verti and projected wind
             Xmesh[::skip_barbs_y, ::skip_barbs_x], 
             alti[::skip_barbs_y, ::skip_barbs_x], 
             #Here dimensions are in the proper order
-            section_ds['PROJ'][::skip_barbs_x, ::skip_barbs_y].T, 
-            section_ds['WT'][::skip_barbs_x, ::skip_barbs_y].T, 
+            section_diff['PROJ'][::skip_barbs_x, ::skip_barbs_y].T, 
+            section_diff['WT'][::skip_barbs_x, ::skip_barbs_y].T, 
             pivot='middle',
             scale=150/arrow_size,  # arrows scale, if higher, smaller arrows
             )
     #add arrow scale in top-right corner
-    u_max = abs(section_ds['PROJ'][::skip_barbs_x, ::skip_barbs_y]).max()
+    u_max = abs(section_diff['PROJ'][::skip_barbs_x, ::skip_barbs_y]).max()
     ax[0].quiverkey(Q, 0.8, 0.9, 
                     U=u_max, 
                     label=str((np.round(u_max, decimals=1)).data) + 'm/s', 
@@ -264,14 +273,14 @@ ax[0].set_xticklabels(list(abscisse_sites.values()),
 #ax.set_xticklabels(abscisse_coords[::10], rotation=0, fontsize=9)
 
 # set y limits (height ASL)
-min_ZS = section_ds['ZS'].min()
+min_ZS = section_diff['ZS'].min()
 ax[0].set_ylim([min_ZS, max_ZS + toplevel])
 ax[0].set_ylabel(ylabel)
 
 
 ### --- Subplot of surface characteristic ---
 
-data_soil = section_ds[surf_var][:, :2]  #keep 2 equivalent levels for plot
+data_soil = section_diff[surf_var][:, :2]  #keep 2 equivalent levels for plot
 p9 = ax[1].pcolor(data_soil.i_sect, 
                   data_soil.level, 
                   data_soil.transpose(), cmap='RdYlGn',
